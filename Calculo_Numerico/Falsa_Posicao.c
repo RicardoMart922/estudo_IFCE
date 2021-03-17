3812d592b538df9966c2bfed27216709e2706993
Ricardo Martins <78050457+RicardoMart922@users.noreply.github.com>
2021-03-17 13:31:59 -0300

    Cálculo Numérico

--- Calculo_Numerico/Falsa_Posicao.c
+++ Calculo_Numerico/Falsa_Posicao.c
@@ -89,8 +89,7 @@ double Funcao(double x) {
 
 double Modulo(double valor) {
     if (valor < 0) {
-        valor = (valor * (-1));
-        return valor;
+        return valor* (-1);
     } else {
         return valor;
     }
